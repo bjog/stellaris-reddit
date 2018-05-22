56094aa4cf519f718dd4b951d1be37598f7ce3e2
Bhavish Jogeeah <bhavishj@hotmail.com>
2018-05-22 20:56:18 +0100

    Search /r/stellaris for last Dev Diary post in this week

--- reddit-bot.py
+++ reddit-bot.py
@@ -3,11 +3,19 @@ import praw
 def search_dev_diary():
 	reddit = praw.Reddit("GCRedditBot")
 	subreddit = reddit.subreddit("stellaris")
-	submissions = subreddit.search("stellaris dev diary", sort="new", time_filter= "week")
+	submissions = subreddit.search("stellaris dev diary", sort = "new",time_filter= "week")
+
 	for submission in submissions:
-		print(submission.title)
+		if(submission.link_flair_text == "Dev diary"):
+			return submission
+	return None
 
 def main():
-	search_dev_diary()
+	submission = search_dev_diary()
+	if(submission == None):
+		print("no dev diary found.")
+	else:
+		print(submission.url)
+
 if __name__ == "__main__":
 	main()	
